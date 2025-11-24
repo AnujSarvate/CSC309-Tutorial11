251c9e0e2140c3b3b811a964694b118a8d18aa28
anuj <anujsarvate11@gmail.com>
2025-11-23 21:39:07 -0500

    Initial commit for T11

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import routes from "./routes.js";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// Load environment variables from .env
+dotenv.config();
+
+const app = express();
+
+// Allow only your frontend origin
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+  })
+);
+
+app.use(express.json());
+app.use("", routes);
+
+export default app;
